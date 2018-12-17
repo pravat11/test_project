959422d6022fb16e286e9fdbca541dc96f0c293a
Prabhat Gautam <prabhatgautam@lftechnology.com>
2018-12-17 08:39:41 +0545

    Filter out based on selected fitler

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,11 +1,27 @@
 import React from 'react';
 
+import VisibilityFilters from '../maps/VisibilityFilters';
+
 const TodoList = props => {
+  const todosToDisplay =
+    props.visibilityFilter === VisibilityFilters.ACTIVE
+      ? props.todos.filter(todo => !todo.isCompleted)
+      : props.visibilityFilter === VisibilityFilters.COMPLETED
+      ? props.todos.filter(todo => todo.isCompleted)
+      : props.todos;
+
+  const emptyStateText =
+    props.visibilityFilter === VisibilityFilters.ACTIVE
+      ? 'No active todos'
+      : props.visibilityFilter === VisibilityFilters.COMPLETED
+      ? 'No completed todos'
+      : 'No todos available';
+
   return (
     <div className="list-wrapper">
       <ul>
-        {!props.todos.length && <li>No todos available</li>}
-        {props.todos.map((todo, index) => {
+        {!todosToDisplay.length && <li>{emptyStateText}</li>}
+        {todosToDisplay.map((todo, index) => {
           const { text, isCompleted } = todo;
 
           // Alternative approach although above one is recommended.
@@ -18,7 +34,7 @@ const TodoList = props => {
                 &times;
               </span>
               <span className="action-buttons complete-button" onClick={() => props.toggleTodoCompletionStatus(text)}>
-                &#10004;
+                {isCompleted ? <React.Fragment>&#8630;</React.Fragment> : <React.Fragment>&#10004;</React.Fragment>}
               </span>
             </li>
           );
