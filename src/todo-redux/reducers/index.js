bea0eccf953a514598de0f0d729b26b2cadae3b1
Prabhat Gautam <prabhatgautam@lftechnology.com>
2018-12-26 08:09:16 +0545

    Add reducer functions

--- src/todo-redux/reducers/index.js
+++ src/todo-redux/reducers/index.js
@@ -1,22 +1,9 @@
 const INITIAL_STATE = {
-  isLoading: false,
   todos: []
 };
 
 export default function rootReducer(state = INITIAL_STATE, action) {
   switch (action.type) {
-    case 'FETCH_STARTED':
-      return {
-        ...state,
-        isLoading: true
-      };
-
-    case 'FETCH_FINISHED':
-      return {
-        ...state,
-        isLoading: false
-      };
-
     case 'ADD_TODO':
       return {
         ...state,
@@ -28,6 +15,42 @@ export default function rootReducer(state = INITIAL_STATE, action) {
             isCompleted: false
           }
         ]
+      };
+
+    case 'EDIT_TODO':
+      return {
+        ...state,
+        todos: state.todos.map(todo => {
+          if (todo.id === action.payload.id) {
+            return {
+              ...todo,
+              text: action.payload.text
+            };
+          }
+
+          return todo;
+        })
+      };
+
+    case 'REMOVE_TODO':
+      return {
+        ...state,
+        todos: state.todos.filter(todo => todo.id !== action.payload)
+      };
+
+    case 'TOGGLE_COMPLETION_STATUS':
+      return {
+        ...state,
+        todos: state.todos.map(todo => {
+          if (todo.id === action.payload) {
+            return {
+              ...todo,
+              isCompleted: !todo.isCompleted
+            };
+          }
+
+          return todo;
+        })
       };
 
     default:
