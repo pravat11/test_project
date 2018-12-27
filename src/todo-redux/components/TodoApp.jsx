2af72db662c3e111de329f04461befd253186101
Prabhat Gautam <prabhatgautam@lftechnology.com>
2018-12-27 08:02:57 +0545

    Connect todoapp to redux store

--- src/todo-redux/components/TodoApp.jsx
+++ src/todo-redux/components/TodoApp.jsx
@@ -1,23 +1,39 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 import TodoList from './TodoList';
 import TodoTabs from './TodoTabs';
 import AddTodoForm from './AddTodoForm';
+import { toggleTodoFormShownStatus } from '../actions/ui';
 
-class App extends React.Component {
-  render() {
-    return (
-      <div className="container">
-        <h1>My todos</h1>
-        <AddTodoForm />
-        <React.Fragment>
-          <button className="add-button">+</button>
-          <TodoTabs />
-          <TodoList />
-        </React.Fragment>
-      </div>
-    );
-  }
-}
+const App = props => (
+  <div className="container">
+    <h1>My todos</h1>
+    {props.isShowingTodoForm ? (
+      <AddTodoForm />
+    ) : (
+      <React.Fragment>
+        <button className="add-button" onClick={props.toggleTodoFormShownStatus}>
+          +
+        </button>
+        <TodoTabs />
+        <TodoList />
+      </React.Fragment>
+    )}
+  </div>
+);
 
-export default App;
+const mapStateToProps = state => ({
+  isShowingTodoForm: state.ui.isShowingTodoForm
+});
+
+const mapDispatchToProps = {
+  toggleTodoFormShownStatus
+};
+
+const enhance = connect(
+  mapStateToProps,
+  mapDispatchToProps
+);
+
+export default enhance(App);
